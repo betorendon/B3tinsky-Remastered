46f9d133a434d6887eb53e10b00e4de8476d13d6
Beto Rendon <betorendonruiz@gmail.com>
2020-06-30 20:38:38 -0500

    hopeful minor changes (css theme and an image)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,3 @@
-require("prismjs/themes/prism-space.css")
+require("./src/styles/themes/prism-space.css")
 // require("prismjs/plugins/command-line/prism-command-line.css")
 // require("prismjs/plugins/line-numbers/prism-line-numbers.css")
